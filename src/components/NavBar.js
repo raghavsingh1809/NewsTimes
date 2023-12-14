609da4e71c0392a2463d8e06615846e6a9fa3da8
Raghvendra Singh <raghvendra18singh@gmail.com>
2023-12-14 10:55:50 +0530

    hg
    ghjg

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,10 +10,7 @@ export class NavBar extends Component {
       <div>
          <nav className="navbar navbar-expand-lg bg-dark ">
          <div className="container-fluid">
-           <Link className="navbar-brand text-light" to="/">NewsTimes</Link>
-           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span className="navbar-toggler-icon"></span>
-           </button>
+           <Link className="navbar-brand text-light" to="/">NewsTimes</Link> 
            <div className="collapse navbar-collapse" id="navbarSupportedContent">
              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                <li className="nav-item"> <Link className="nav-link active text-light" to="/">Home</Link></li>  
